3f9032d2f97cb6d7440ed15db88505d3cbf4ce57
Sophie Peneva <39370712+sophiepeneva@users.noreply.github.com>
2018-05-28 22:30:28 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -22,6 +22,24 @@ public class Main {
                 ReverseWords rw1 = new ReverseWords(ci.getCommand(),ci.getWords());
                 System.out.println(rw1.ReverseOrder());
             }
+            if(action.compareTo("set")==0){
+                Set s1 = new Set(ci.getCommand(),ci.getWords());
+                if(s1.getType().compareTo("number")==0||s1.getType().compareTo("String")==0)
+                {
+                    s1.Set();
+                    ci.addToVar(s1.getVars());
+                }
+                else{ System.out.println("Err");}
+            }
+            if(action.compareTo("get")==0){
+               Get g1 = new Get(ci.getCommand(),ci.getWords());
+                System.out.println(g1.Get(ci));
+            }
+            if(action.compareTo("calc")==0){
+                Calc c1 = new Calc(ci.getCommand(),ci.getWords(),ci);
+                c1.Calc(ci);
+            }
         }
     }
 }
+
