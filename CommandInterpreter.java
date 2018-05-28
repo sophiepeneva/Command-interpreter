e203db32aca55157ca7396aeded6909d75800f23
Sophie Peneva <39370712+sophiepeneva@users.noreply.github.com>
2018-05-28 22:31:42 +0300

    Update CommandInterpreter.java

--- CommandInterpreter.java
+++ CommandInterpreter.java
@@ -3,16 +3,70 @@
 public class CommandInterpreter {
     private String command;
     private String words;
+    private Variable vars[];
+    private int numOfVars;
     public CommandInterpreter(String command)
     {
         this.command = command;
         this.separateWords();
         this.getAction();
+        vars = new Variable[0];
+        numOfVars=0;
+    }
+    public void setTypeAndValueByName(Variable var)
+    {
+        for (int i = 0; i < numOfVars; i++) {
+            if(var.getName().compareTo(vars[i].getName())==0)
+            {
+                var.setValue(vars[i].getValue());
+                var.setType(vars[i].getType());
+            }
+        }
+
+    }
+    public int getNumOfVars(){return numOfVars;}
+    public void addToVar(Variable var) {
+        int length = numOfVars;
+        boolean hasItAlready = false;
+        for (int i = 0; i < length; i++) {
+            if(vars[i].getName().compareTo(var.getName())==0)
+            {
+                hasItAlready=true;
+                System.out.println(var.getValue());
+                vars[i].setValue(var.getValue());
+            }
+        }
+        if(hasItAlready==false) {
+            Variable[] temp = new Variable[length];
+            for (int i = 0; i < length; i++) {
+                temp[i] = vars[i];
+            }
+            vars = new Variable[length + 1];
+            for (int i = 0; i < length; i++) {
+                vars[i] = temp[i];
+            }
+            vars[length] = new Variable();
+            vars[length].setName(var.getName());
+            vars[length].setValue(var.getValue());
+            vars[length].setType(var.getType());
+            numOfVars++;
+        }
+    }
+
+    public Variable getVar(int i){return vars[i];}
+
+    public void displayVars()
+    {
+        for (int i = 0; i < numOfVars; i++) {
+            System.out.println(vars[i].getName()+"="+vars[i].getValue()+" and is a "+vars[i].getType());
+        }
+
     }
     public CommandInterpreter()
     {
         this.command = "No command";
         this.words = "No words";
+        vars = new Variable[0];
     }
     String getCommand() { return command; }
     String getWords() { return words; }
