cc4c99964d13a6046272bd919a4e3c6faa6a16a5
Sophie Peneva <39370712+sophiepeneva@users.noreply.github.com>
2018-05-28 22:31:12 +0300

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,27 @@
+# Command-interpreter
+
+public class Variable {
+    private String name;
+    private String value;
+    private String type;
+
+    public Variable(String name, String value, String type)
+    {
+        this.value = value;
+        this.name = name;
+        this.type = type;
+    }
+    public Variable()
+    {
+        value = "";
+        name = "";
+        type = "";
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getValue() { return value; }
+    public void setValue(String value) { this.value = value; }
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+}
